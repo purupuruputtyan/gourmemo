95547e3bee9716bd81c39de6d846cca3afa485ae
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-05-10 08:39:59 +0000

    [Add]管理者側検索機能

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,17 +1,17 @@
 class Admin::SearchesController < ApplicationController
-  before_action :authenticate_sdmin!
-  
+  before_action :authenticate_admin!
+
   def search
     @model = params[:model]
     @content = params[:content]
     @method = params[:method]
     if @model == 'user'
-      @records = User.search_for(@content, @method)
+      @records = User.search_for(@content, @method).page(params[:page])
     elsif @model == 'post'
-      @records = Post.search_for(@content, @method)
+      @records = Post.search_for(@content, @method).page(params[:page])
     #elsif @model == 'tag'
 		#	@records = Tag.search_books_for(@content, @method)
     end
   end
-  
+
 end
