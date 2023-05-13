e26be687cc5175d3e78e4e00aeb0a4b832dc5fce
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-05-13 06:35:30 +0000

    [Fix]投稿一覧画面レイアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_classname
+
+  def set_classname
+    if params[:controller] =='public/homes' && params[:action] =='top'
+      @classname = 'topbody'
+    else
+      @classname = 'other'
+    end
+  end
+
 end
