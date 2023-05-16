e93ecdcef990a9b1d7ae64cc988d159399ba0959
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-05-16 05:01:17 +0000

    [Add]管理者コメント削除機能非同期化

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,9 +2,9 @@ class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
 
   def destroy
-    comment = Comment.find(params[:id])
-    comment.destroy
-    redirect_to request.referer
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    #redirect_to request.referer
   end
 
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,7 +9,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page])
+    @posts = @user.posts.page(params[:page]).per(6)
   end
 
   def edit
