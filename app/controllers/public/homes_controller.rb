77d56bdd3a554c6a323ac6214519175e58aa8a1c
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-05-27 07:51:27 +0000

    [Fix]トップページ不具合修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -9,24 +9,37 @@ class Public::HomesController < ApplicationController
     #「&」はif current_user endで囲っているのと同じ役割を果たしてくれている。
     released_user_ids = User.where(status: :released).or(User.where(id: current_user&.id)).pluck(:id)
 
-    #過去１週間に1件も投稿がない場合、topに投稿が表示されなくなるため、投稿が０の時はweek_postsメソッドを使わない
-    if Post.week_posts.present?
-      #postモデルにweek_postsメソッドを記述。この過去１週間の作成日時を抽出している。
-      #postテーブルから過去１週間の作成日時を抽出し、さらにそこからアクティブなユーザーだけを探している
-      #eager_loadでfavoriteテーブルを外部結合し、groupでpostのidを基準にして、、order内でfavoriteテーブルのpost_idをカウントしている
-      @posts = Post.week_posts.where(user_id: released_user_ids)
-            .eager_load(:favorites)
-            .group('posts.id')
-            .order('count(favorites.post_id) DESC')
-            .page(params[:page])
-    else
-      #１週間以内に投稿が０だった場合は全投稿のいいね多い順を表示
-      @posts = Post.where(user_id: released_user_ids)
-            .eager_load(:favorites)
-            .group('posts.id')
-            .order('count(favorites.post_id) DESC')
-            .page(params[:page])
+    # Postの公開されている記事を取得する。
+    @posts = Post.where(user_id: released_user_ids)
+
+    # その中から１週間のうちに投稿されたデータがあればそのデータのみ取得する。
+    if @posts.week_posts.present?
+      @posts = @posts.week_posts
     end
+
+    # その中からいいねがついているデータを取得する。
+    @posts = @posts.eager_load(:favorites).group('posts.id').order('count(favorites.post_id) DESC')
+
+    @posts = @posts.page(params[:page])
+
+#    #過去１週間に1件も投稿がない場合、topに投稿が表示されなくなるため、投稿が０の時はweek_postsメソッドを使わない
+#    if Post.week_posts.present?
+#      #postモデルにweek_postsメソッドを記述。この過去１週間の作成日時を抽出している。
+#      #postテーブルから過去１週間の作成日時を抽出し、さらにそこからアクティブなユーザーだけを探している
+#      #eager_loadでfavoriteテーブルを外部結合し、groupでpostのidを基準にして、、order内でfavoriteテーブルのpost_idをカウントしている
+#      @posts = Post.week_posts.where(user_id: released_user_ids)
+#            .eager_load(:favorites)
+#            .group('posts.id')
+#            .order('count(favorites.post_id) DESC')
+#            .page(params[:page])
+#    else
+#      #１週間以内に投稿が０だった場合は全投稿のいいね多い順を表示
+#      @posts = Post.where(user_id: released_user_ids)
+#            .eager_load(:favorites)
+#            .group('posts.id')
+#            .order('count(favorites.post_id) DESC')
+#            .page(params[:page])
+#    end
   end
 
   def about
